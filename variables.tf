160d8cbef6f25c74c47a3f0243680555125c2346
Bartosz Dopke <bartosz.dopke@nordcloud.com>
2023-08-03 15:33:10 +0200

    fd

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "access_key" {
-  type = string
-}
-
-variable "secret_key" {
-  type = string
-}
-
 variable "region" {
   type        = string
   description = "AWS Region (being set using environment variable TF_VAR_region)"
