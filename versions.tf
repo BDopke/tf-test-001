ef35f02caa4be752ec5d6487701991432868b9f2
Bartosz Dopke <bartosz.dopke@nordcloud.com>
2023-08-04 10:36:35 +0200

    ds

--- versions.tf
+++ versions.tf
@@ -2,9 +2,13 @@ terraform {
   required_version = ">= 1.0.11"
 
   required_providers {
-    aws    = ">= 4.8.0"
-    archive = ">= 2.4.0"
+    aws    = "~= 4.8.0"
+    archive = "~= 2.4.0"
   }
 }
 
-provider "aws" {}
+provider "aws" {
+  region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
